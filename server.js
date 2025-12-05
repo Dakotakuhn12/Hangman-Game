f962698b43aad65ed3f55b20aacd301a32636c66
Dakota Kuhn <dakotakuhn27@gmail.com>
2025-12-05 12:48:15 -0500

    save
    Co-authored-by: Coby Hughes <c-o-b-a-x@users.noreply.github.com>
    Co-authored-by: jsperry0807 <jsperry0807@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
+import http from "http"
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
 
 const port = 3000;
 
@@ -8,12 +10,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const server = http.createServer(app)
+const io = new Server(server);
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/hangman.html"));
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
